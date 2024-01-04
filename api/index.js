1c0c421c30b4d2d6931821f195e2f9e871a92135
Papsanly <papsanlysenko@gmail.com>
2024-01-04 22:48:09 +0200

    removed listening

--- api/index.js
+++ api/index.js
@@ -23,9 +23,4 @@ process.on('SIGTERM', () => {
   })
 })
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
 export default server
