40bd41e1202b2c9094ccc541bac9385bbac5339a
Papsanly <papsanlysenko@gmail.com>
2024-01-04 23:09:41 +0200

    added separate api folder

--- api/index.js
+++ api/index.js
@@ -1,26 +1,5 @@
-import http from 'http'
-import routes from './routes.js'
+import handler from '../src/index.js'
 
-const server = http.createServer((req, res) => {
-  const { method, url } = req
-  console.log(`Received ${method} request for ${url}`)
-
-  const handler = routes[url] ? routes[url][method] : null
-
-  if (handler) {
-    handler(req, res)
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' })
-    res.end('404 Not Found')
-  }
-})
-
-process.on('SIGTERM', () => {
-  console.log('Starting graceful shutdown')
-  server.close(() => {
-    console.log('All connections closed')
-    process.exit(0)
-  })
-})
-
-export default server
+export default function apiHandler(req, res) {
+  return handler(req, res)
+}
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import http from 'http'
+import routes from './routes.js'
+
+function handler(req, res) {
+  const { method, url } = req
+  console.log(`Received ${method} request for ${url}`)
+
+  const handler = routes[url] ? routes[url][method] : null
+
+  if (handler) {
+    handler(req, res)
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('404 Not Found')
+  }
+}
+
+const server = http.createServer(handler)
+
+process.on('SIGTERM', () => {
+  console.log('Starting graceful shutdown')
+  server.close(() => {
+    console.log('All connections closed')
+    process.exit(0)
+  })
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+export default handler
