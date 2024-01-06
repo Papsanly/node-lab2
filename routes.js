01bc52964edb08124cd1f199296f00278c235cba
Papsan <papsanlysenko@gmail.com>
2024-01-06 19:07:38 +0200

    added handling of different content types

--- routes.js
+++ routes.js
@@ -26,8 +26,25 @@ export default {
         body += chunk.toString()
       })
       req.on('end', () => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end(`Received data: ${body}`)
+        const contentType = req.headers['content-type']
+        let parsedData
+
+        if (contentType === 'application/json') {
+          try {
+            parsedData = JSON.parse(body)
+          } catch (error) {
+            res.writeHead(400, { 'Content-Type': 'text/plain' })
+            return res.end('Invalid JSON')
+          }
+        } else if (contentType === 'application/x-www-form-urlencoded') {
+          parsedData = new URLSearchParams(body)
+        } else {
+          res.writeHead(415, { 'Content-Type': 'text/plain' })
+          return res.end('Unsupported Media Type')
+        }
+
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(`Received data: ${JSON.stringify(parsedData)}`)
       })
     }
   }
